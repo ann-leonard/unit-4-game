4227aa8026e4ef38823490201cf84e6caddc3404
ann-leonard <aleo0779@gmail.com>
2019-02-27 00:04:41 -0500

    finished game

--- javascript/game.js
+++ javascript/game.js
@@ -1,50 +1,120 @@
 
 var images = $(".crystals");
-var targetNumber = randRange(19,120)
-var imageNumbers = [];
+var targetNumber = randRange(19, 120)
 var score = 0;
-var listMultiples=multiplesOfTargetNumber(targetNumber);
+var listMultiples = multiplesOfTargetNumber(targetNumber);
+var imageValues = getImgNumbers();
+$("#tryAgain").hide();
 
-var image_index=[0,1,2,3]
- 
-function assignImageNumbers(list){
-    randomizedImageIndex=[]
-    for (x=0;x<images.length;x++){
-        random_index=randRange(0,list.length)
-        randomizedImageIndex.push(list[random_index])
-        list.pop(random_index)
-    }
-    return randomizedImageIndex
+function reset(){
+    targetNumber= randRange(19,120)
+    score=0
+    listMultiples=multiplesOfTargetNumber(targetNumber)
+    imageValues=getImgNumbers();
+    $("#tryAgain").hide();
+    $("#domTarget").text("Your number is: " + targetNumber)
+    $("#domTarget").removeClass("bg-success bg-danger")
+    $("#domScore").text("Score: " + score)
+
 }
-function randRange(min,max){
+
+function randomShuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function randomChoice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+function multiplesOfTargetNumber(n) {
+    var multiples = []
+    //iterates over every number until targetNumber and checks for multiples
+    for (x = 0; x < n; x++) {
 
-
-
-function multiplesOfTargetNumber(n){
-   var multiples = []
-    //iterates over every number until targetNumber
-    for (x=0; x<n;x++){ 
-
-        if (n % x === 0){
-            multiples.push(x)}
+        if (n % x === 0) {
+            multiples.push(x)
+        }
     }
-    //if (listMultiples >= 4){
-     //   listMultiples = imageNumbers;
-    
-    //if (imageNumbers > 4){
-    //       imageNumbers.pop();
-    //    }
-    //}
+
     return multiples
 }
+// makes a list with 4 elements; three random values and one multiple of target number;
+function getImgNumbers() {
+    var numbers = [];
+    for (i = 0; i < images.length - 1; i++) {
+        numbers.push(randRange(2, 19))
+    }
+    numbers.push(randomChoice(listMultiples))
+    numbers = randomShuffle(numbers);
+    return numbers
+}
 
+function winConditions() {
+    if (score === targetNumber) {
+        $("#domTarget").addClass("bg-success")
+        $("#domTarget").text("You win!")
+        imageValues = [0, 0, 0, 0]
+        $("#tryAgain").show();
 
+    }
+    if (score > targetNumber) {
+        $("#domTarget").addClass("bg-danger")
+        $("#domTarget").text("You lose :(")
+        imageValues = [0, 0, 0, 0]
+        $("#tryAgain").show();
 
-$(images[0]).on("click",function(){
-    score += imageNumbers[0]
+    }
+}
+
+$("#domTarget").text("Your number is: " + targetNumber)
+
+$(images[0]).on("click", function () {
+    score += imageValues[0]
+    console.log(score)
+    $("#domScore").text("Score: " + score)
+    winConditions()
 })
 
+$(images[1]).on("click", function () {
+    score += imageValues[1]
+    console.log(score)
+    $("#domScore").text("Score: " + score)
+    winConditions()
+})
 
+$(images[2]).on("click", function () {
+    score += imageValues[2]
+    console.log(score)
+    $("#domScore").text("Score: " + score)
+    winConditions()
+})
+
+$(images[3]).on("click", function () {
+    score += imageValues[3]
+    console.log(score)
+    $("#domScore").text("Score: " + score)
+    winConditions()
+})
+
+$("#tryAgain").on("click", function(){
+    reset();
+})

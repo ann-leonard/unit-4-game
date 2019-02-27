4f5bd11fada4a937b7cbb367d423dce8233587de
ann-leonard <aleo0779@gmail.com>
2019-02-27 10:22:24 -0500

    formatting/documentation

--- javascript/game.js
+++ javascript/game.js
@@ -1,23 +1,23 @@
 
 var images = $(".crystals");
-var targetNumber = randRange(19, 120)
+var targetNumber = randRange(19, 120);
 var score = 0;
 var listMultiples = multiplesOfTargetNumber(targetNumber);
 var imageValues = getImgNumbers();
 $("#tryAgain").hide();
 
-function reset(){
-    targetNumber= randRange(19,120)
-    score=0
-    listMultiples=multiplesOfTargetNumber(targetNumber)
-    imageValues=getImgNumbers();
+function reset() {
+    targetNumber = randRange(19, 120);
+    score = 0;
+    listMultiples = multiplesOfTargetNumber(targetNumber);
+    imageValues = getImgNumbers();
     $("#tryAgain").hide();
-    $("#domTarget").text("Your number is: " + targetNumber)
-    $("#domTarget").removeClass("bg-success bg-danger")
-    $("#domScore").text("Score: " + score)
+    $("#domTarget").text("Your number is: " + targetNumber);
+    $("#domTarget").removeClass("bg-success bg-danger");
+    $("#domScore").text("Score: " + score);
 
 }
-
+//shuffles the items in an array
 function randomShuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -37,84 +37,85 @@ function randomShuffle(array) {
     return array;
 }
 
+//returns a random item of an array
 function randomChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+//returns a random integer within a set range 
 function randRange(min, max) {
-    return Math.floor(Math.random() * (max - min) + min)
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
+//returns a list of multiples of the target number to ensure the user has a chance of winning
 function multiplesOfTargetNumber(n) {
-    var multiples = []
+    var multiples = [];
     //iterates over every number until targetNumber and checks for multiples
     for (x = 0; x < n; x++) {
 
         if (n % x === 0) {
-            multiples.push(x)
+            multiples.push(x);
         }
     }
 
-    return multiples
+    return multiples;
 }
 // makes a list with 4 elements; three random values and one multiple of target number;
 function getImgNumbers() {
     var numbers = [];
     for (i = 0; i < images.length - 1; i++) {
-        numbers.push(randRange(2, 19))
+        numbers.push(randRange(2, 19));
     }
-    numbers.push(randomChoice(listMultiples))
+    numbers.push(randomChoice(listMultiples));
     numbers = randomShuffle(numbers);
-    return numbers
-}
+    return numbers;
+};
 
+// checks for win or lose and stops allowing points to be added to score
 function winConditions() {
     if (score === targetNumber) {
-        $("#domTarget").addClass("bg-success")
-        $("#domTarget").text("You win!")
-        imageValues = [0, 0, 0, 0]
+        $("#domTarget").addClass("bg-success");
+        $("#domTarget").text("You win!");
+        imageValues = [0, 0, 0, 0];
         $("#tryAgain").show();
 
     }
     if (score > targetNumber) {
-        $("#domTarget").addClass("bg-danger")
-        $("#domTarget").text("You lose :(")
-        imageValues = [0, 0, 0, 0]
+        $("#domTarget").addClass("bg-danger");
+        $("#domTarget").text("You lose :(");
+        imageValues = [0, 0, 0, 0];
         $("#tryAgain").show();
 
     }
-}
+};
+//displays target number for user
+$("#domTarget").text("Your number is: " + targetNumber);
 
-$("#domTarget").text("Your number is: " + targetNumber)
-
+//onclick events 
 $(images[0]).on("click", function () {
-    score += imageValues[0]
-    console.log(score)
-    $("#domScore").text("Score: " + score)
-    winConditions()
-})
+    score += imageValues[0];
+    $("#domScore").text("Score: " + score);
+    winConditions();
+});
 
 $(images[1]).on("click", function () {
-    score += imageValues[1]
-    console.log(score)
-    $("#domScore").text("Score: " + score)
-    winConditions()
-})
+    score += imageValues[1];
+    $("#domScore").text("Score: " + score);
+    winConditions();
+});
 
 $(images[2]).on("click", function () {
-    score += imageValues[2]
-    console.log(score)
-    $("#domScore").text("Score: " + score)
-    winConditions()
-})
+    score += imageValues[2];
+    $("#domScore").text("Score: " + score);
+    winConditions();
+});
 
 $(images[3]).on("click", function () {
-    score += imageValues[3]
-    console.log(score)
-    $("#domScore").text("Score: " + score)
-    winConditions()
-})
-
-$("#tryAgain").on("click", function(){
+    score += imageValues[3];
+    $("#domScore").text("Score: " + score);
+    winConditions();
+});
+// onclick event to call reset function to start a new game 
+$("#tryAgain").on("click", function () {
     reset();
-})
+});
